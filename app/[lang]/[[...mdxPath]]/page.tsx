799bd67c31e2167343313454fe4267e0fe0de513
cha777 <chathuranga_wm@yahoo.com>
2025-02-01 20:25:01 +0530

    Localization support

--- app/[lang]/[[...mdxPath]]/page.tsx
+++ app/[lang]/[[...mdxPath]]/page.tsx
@@ -14,7 +14,7 @@ export const generateStaticParams = generateStaticParamsFor('mdxPath');
 
 export async function generateMetadata(props: PageProps) {
   const params = await props.params;
-  const { metadata } = await importPage(params.mdxPath);
+  const { metadata } = await importPage(params.mdxPath, params.lang);
 
   return metadata;
 }
@@ -23,7 +23,7 @@ const Wrapper = useMDXComponents().wrapper;
 
 export default async function Page(props: PageProps) {
   const params = await props.params;
-  const result = await importPage(params.mdxPath);
+  const result = await importPage(params.mdxPath, params.lang);
   const { default: MDXContent, toc, metadata } = result;
 
   return (
--- next.config.ts
+++ next.config.ts
@@ -14,10 +14,15 @@ const nextConfig: NextConfig = {
   },
   basePath: process.env.BASE_PATH,
   assetPrefix: process.env.BASE_PATH,
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+  },
 };
 
 const withNextra = nextra({
   // ... Other Nextra config options
+  unstable_shouldAddLocaleToLinks: true,
 });
 
 export default withNextra(nextConfig);
